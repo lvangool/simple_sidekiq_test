3d9fbf2827737acd50d87b88da82f356bed99af6
Luvic van Gool <vic@thecloudblocks.com>
2013-03-13 12:17:15 +0000

    worker

--- app/worker/my_worker.rb
+++ app/worker/my_worker.rb
@@ -5,6 +5,6 @@ class MyWorker
 		File.open("/tmp/sidekiq_test_output.txt", 'a+') do |f2|
 			f2.puts "#{Time.now.strftime('%I:%M:%S %z')} RAILS_ENV='#{ENV['RAILS_ENV']}'"
 		end
-
+		MyWorker.perform_in(5.seconds)
 	end
 end
