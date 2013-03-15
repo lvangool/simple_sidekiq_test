cbe95e0cf15ecb9f10ce08a64c110c33e98c1d1d
Luvic van Gool <vic@thecloudblocks.com>
2013-03-15 10:25:24 +0000

    sleep

--- app/worker/my_worker.rb
+++ app/worker/my_worker.rb
@@ -4,7 +4,10 @@ class MyWorker
 	def perform
 		File.open("/tmp/sidekiq_test_output.txt", 'a+') do |f2|
 			f2.puts "#{Time.now.strftime('%I:%M:%S %z')} SAMPLE_ENV='#{ENV['SAMPLE_ENV']}'"
-		end
-		MyWorker.perform_in(5.seconds)
+			f2.puts "sleeping for 10 seconds"
+			sleep 10
+			f2.puts "Woken up, rescheduling"
+		end		
+		MyWorker.perform_async
 	end
 end
