6a1c22de2427a736b99f49b710d4533aa46a62c7
Luvic van Gool <vic@thecloudblocks.com>
2013-03-15 11:03:31 +0000

    sleep

--- app/worker/my_worker.rb
+++ app/worker/my_worker.rb
@@ -8,10 +8,7 @@ class MyWorker
 		File.open("/tmp/sidekiq_test_output.txt", 'a+') do |f2|
 			f2.puts "#{Time.now.strftime('%I:%M:%S %z')} Picked up (#{ENV['RAILS_ENV']}) -> going to sleep for #{SECONDS} seconds"
 		end
-		sleep 10
-		File.open("/tmp/sidekiq_test_output.txt", 'a+') do |f2|
-			f2.puts "#{Time.now.strftime('%I:%M:%S %z')} Woken up -> rescheduling"
-		end
+		sleep SECONDS
 		MyWorker.perform_async
 	end
 end
