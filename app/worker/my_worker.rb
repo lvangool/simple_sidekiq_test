fac38df195f5163b21dcb2225a5d7208a94c07d9
Luvic van Gool <vic@thecloudblocks.com>
2013-03-15 11:14:42 +0000

    sleep

--- app/worker/my_worker.rb
+++ app/worker/my_worker.rb
@@ -2,13 +2,14 @@
 class MyWorker
 
 	include Sidekiq::Worker
-	SECONDS = 120
+	SECONDS = 300
 
 	def perform
 		File.open("/tmp/sidekiq_test_output.txt", 'a+') do |f2|
 			f2.puts "#{Time.now.strftime('%I:%M:%S %z')} Picked up (#{ENV['RAILS_ENV']}) -> going to sleep for #{SECONDS} seconds"
 		end
 		sleep SECONDS
+		f2.puts "#{Time.now.strftime('%I:%M:%S %z')} Woken up (#{ENV['RAILS_ENV']}) -> going to reschedule"
 		MyWorker.perform_async
 	end
 end
